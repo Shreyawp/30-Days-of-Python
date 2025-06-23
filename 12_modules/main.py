c789ad15731bce4f4d1c0aa6725027bce15e33fc
Shreyawp <shreya.parse22@gmail.com>
2025-06-23 19:23:15 +0530

    Adding modules main file

--- 12_modules/main.py
+++ 12_modules/main.py
@@ -14,26 +14,5 @@ weight = 294.0
 ##########################################################
 '''
 
-# OS module
-import os
-cwd = os.getcwd()
-print("Current wokring directory: ", cwd)
- 
-####################### OUTPUT ##########################
-# Current wokring directory:  C:\Users\Admin\OneDrive\Desktop\python\30 days with vscode
-##########################################################
-
-os.chdir('12_modules')                           #Change directory
-print("Current wokring directory: ", os.getcwd()) 
-####################### OUTPUT ##########################
-# >> Current wokring directory:  C:\Users\Admin\OneDrive\Desktop\python\30 days with vscode\12_modules
-##########################################################
-
-path = os.path.join(cwd, "dummy_dir")  
-os.mkdir(path)         #Create new directory
-print("Created new directory")
-####################### OUTPUT ##########################
-#>> Created new directory
-##########################################################
-
-
+# Some of the common built-in modules: 
+# math, datetime, os,sys, random, statistics, collections, json,re
