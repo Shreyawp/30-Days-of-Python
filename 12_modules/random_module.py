35b8efaaf79d3c5ffec889ccf9113f50622ba143
Shreyawp <shreya.parse22@gmail.com>
2025-06-26 20:34:11 +0530

    modifies random module

--- 12_modules/random_module.py
+++ 12_modules/random_module.py
@@ -1,162 +1,103 @@
 import random
 
-a = [1,2,3,4,5,6,7,8,9]
-print(random.choice(a))
+# random() method - generates random value between 0 and 1
+value = random.random()
+print(value)
+# >> 0.467527645149853
 
-b = ('a', 56, "abc", 100, '87', 'python')
-print(random.choices(b, k=3))
+# uniform() method - generates random floating point num between given num range
+value = random.uniform(1, 10)
+print(value)
+# >> 6.986557762968738
 
-s = "Python for all"
-print(random.choice(s))
+# randint() method - generates random integer num  between given range
+value = random.randint(0,1)  # example of coin: Heads - 1, Tails - 0
+print(value)
+# >> 1
+
+# choice() method - select one random value from given sequence
+greetings = ['Hello', 'Hi', 'Hey', 'Howdy', 'Hola'] 
+value = random.choice(greetings)
+print(value + ', Eric!')
+# >> Hey, Eric!
+
+# choices() - select more than one or k random value from given sequence 
+# example: one ball has chance of landing in one of the color pockets 
+colors = ['Red', 'Black' ,'Green']
+results = random.choices(colors, k=10)
+print(results)
+# >> ['Red', 'Black', 'Black', 'Red', 'Green', 'Red', 'Black', 'Red', 'Red', 'Red']
+
+# weight attribute for likeliness of each item in sequence
+results = random.choices(colors, weights=[18, 18 ,2], k=10)
+print(results)
+# >> ['Black', 'Red', 'Black', 'Red', 'Black', 'Black', 'Red', 'Red', 'Red', 'Black']
+
+# shuffle() - to shuffle sequence
+# Example : deck of playing cards
+deck = list(range(1,53))
+random.shuffle(deck)
+print(deck)
+# >> [44, 18, 23, 40, 38, 16, 51, 8, 32, 19, 10, 3, 49, 36, 24, 33, 39, 30, 9, 47, 45, 34, 41, 25, 21, 28, 2, 7, 13, 27, 31, 35, 52, 29, 6, 15, 37, 22, 42, 12, 46, 20, 11, 17, 1, 26, 4, 5, 43, 48, 14, 50]
+
+# sample() - to generate k unique samples
+hand = random.sample(deck, k=5)
+print(hand)
+# >> [49, 1, 20, 39, 17]
+
+
+# Example: create random names with their details 
+first_name = ['Harry', 'Ron', 'Hermione', 'Ginny', 'Neville', 'Luna', 'Percy', 'Lucas', ]
+last_name = ['Potter', 'Weasley', 'Granger', 'Longbottom', 'Lovegood', 'Jackson', 'Malfoy']
+
+street_names = ['Daigon Valley', '4th Avenue', 'Main Street', 'Oak', 'Park', 'Pine', 'Elm']
+cities = ['Metropolis', 'South Park', 'Manhattan', 'Sunnydale', 'Metropolis', 'Alantis']
+states = ['AL', 'AR', 'CA', 'DN', 'GA', 'FL', 'HI', 'IN', 'YM', 'AU']
+
+for num in range(5):
+    first = random.choice(first_name)
+    last = random.choice(last_name)
+
+    phone = f'{random.randint(100, 999)}-555-{random.randint(1000, 9999)}'
+
+    street_num = random.randint(100, 999)
+    street = random.choice(street_names)
+    city = random.choice(cities)
+    state = random.choice(states)
+    zip_code = random.randint(10000, 99999)
+    address = f'{street_num} {street} St., {city} {state} {zip_code}'
+
+    email = first.lower() + last.lower() + '@xyzmail.com'
+
+    print(f'{first} {last}\n{phone}\n{address}\n{email}\n')
+
+
 ''' 
 ####################### OUTPUT ##########################
-9
-['abc', '87', 'a']
-o
-##########################################################
-'''
+Percy Weasley
+636-555-3190
+300 Park St., Sunnydale IN 96185
+percyweasley@xyzmail.com
 
-# seed() generates same random number on every run
-print('*'*10, 'seed()','*'*10)
-for i in range(2):
-    random.seed(0)
-    print(random.randint(1,10))
+Percy Granger
+893-555-3049
+569 Main Street St., Sunnydale IN 58757
+percygranger@xyzmail.com
 
-## reproduce same random list
-random.seed()
-print(random.sample(range(1,10),6))
+Ginny Granger
+469-555-9326
+349 Daigon Valley St., Metropolis CA 66417
+ginnygranger@xyzmail.com
 
-## data splitting
-x = list(range(100))
-random.seed(10)
-random.shuffle(a)
-print(a)
-''' 
-####################### OUTPUT ##########################
-********** seed() **********
-7
-7
-[2, 8, 1, 7, 6, 4]
-[6, 3, 8, 2, 9, 5, 4, 7, 1]
-##########################################################
-'''
-# randint() to generate random integers
-print('*'*10, 'randint()','*'*10)
-print(random.randint(5,15))
-print(random.randint(-12, -6))
-''' 
-####################### OUTPUT ##########################
-********** randint() **********
-9
--7
-##########################################################
-'''
+Neville Lovegood
+226-555-1114
+524 Park St., Sunnydale HI 30471
+nevillelovegood@xyzmail.com
 
-# randrange(start,stop,step)
-print('*'*10, 'randrange()','*'*10)
-print('multiple of 3 between 1-51: ', random.randrange(1,51,3))
+Neville Malfoy
+989-555-6177
+721 4th Avenue St., Sunnydale AL 84968
+nevillemalfoy@xyzmail.com
 
-dice_roll = random.randrange(1,7)
-print(f'dice roll: {dice_roll}')
-
-z = [10, 80, 45, 35, 72, 56]
-random_index = random.randrange(len(z))
-print(z[random_index])
-''' 
-####################### OUTPUT ##########################
-********** randrange() **********
-multiple of 3 between 1-51:  16
-dice roll: 1
-72
-##########################################################
-'''
-# getstate() returns an object with the current internal state 
-print('*'*10, 'getstate() & setstate()','*'*10)
-state = random.getstate()
-print(random.sample(range(20), k=10))
-#setstate() method is used to restore the state
-random.setstate(state)
-print(random.sample(range(20), k=5))
-''' 
-####################### OUTPUT ##########################
-********** getstate() & setstate() **********
-[5, 1, 16, 15, 10, 18, 3, 11, 19, 0]
-[5, 1, 16, 15, 10]
-##########################################################
-'''
-
-state1 = random.getstate()
-num = random.random()
-print("random number of captured state: ", num)
-num2 =  random.random()
-print("random number 2 of captured state: ", num2)
-random.setstate(state1)
-num3 = random.random()
-print("random number of previously captured state: ", num3)
-''' 
-####################### OUTPUT ##########################
-random number of captured state:  0.07608934678134815
-random number 2 of captured state:  0.9523992311419316
-random number of previously captured state:  0.07608934678134815
-##########################################################
-'''
-
-# random() generates a float number in the range [0.0, 1.0]
-print('*'*10, 'random()','*'*10)
-from random import random
-print(random())
-lst = []
-for i in range(10):
-    lst.append(random())
-print(lst)
-''' 
-####################### OUTPUT ##########################
-********** random() **********
-0.9523992311419316
-[0.745546698544599, 0.3611586246233909, 0.4203962002706605, 0.138689276599814, 0.35508168773059534, 0.4212639948995278, 0.8268482288343075, 0.26232111049317675, 0.17467058960990223, 0.3031051133710616]
-##########################################################
-'''
-
-# Select Multiple Unique Random Items w/o repeating
-print('*'*10, 'sample()','*'*10)
-from random import sample
-print(sample(a,2))
-print(sample(b,5))
-print(sample(s,3))
-''' 
-####################### OUTPUT ##########################
-********** sample() **********
-[5, 8]
-[100, 56, 'python', 'abc', '87']
-['P', 'r', 'l']
-##########################################################
-'''
-
-# Shuffle Elements in a List
-print('*'*10, 'shuffle()','*'*10)
-from random import shuffle
-print("Before shuffle: ", a)
-shuffle(a)
-print("After shuffle: ", a)
-''' 
-####################### OUTPUT ##########################
-Before shuffle:  [6, 3, 8, 2, 9, 5, 4, 7, 1]
-After shuffle:  [1, 4, 6, 5, 9, 7, 3, 8, 2]
-##########################################################
-'''
-
-# uniform(lower_bound, upper_bound) - generates random floating-point number
-print('*'*10, 'uniform()','*'*10)
-from random import uniform
-print(uniform(1,5))
-
-lat = uniform(-90, 90)
-lon = uniform(-180, 180)
-print(f"Random location: {lat}, {lon}")
-''' 
-####################### OUTPUT ##########################
-********** uniform() **********
-3.6667598053509645
-Random location: -8.887859743195989, -10.809371893123512
 ##########################################################
 '''
