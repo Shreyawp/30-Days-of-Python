fc122bab73f4ae17a61db6d4447c22d45d826182
Shreyawp <shreya.parse22@gmail.com>
2025-11-19 08:53:13 +0530

    Django Rest API tutorial : Serializers and Response Objects

--- 29_Django_Rest_Framework/mysite/api/views.py
+++ 29_Django_Rest_Framework/mysite/api/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-from rest_framework import generics, status
+#from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from api.serializers import ProductSerializer
+from api.models import Product
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serialzer = ProductSerializer(products, many=True)
+    return Response(serialzer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serialzer = ProductSerializer(product)
+    return Response(serialzer.data)
