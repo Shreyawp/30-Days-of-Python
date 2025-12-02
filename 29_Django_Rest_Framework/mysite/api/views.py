a878cbc835e5fc5120f3ddd8f87799ab87185cdd
Shreyawp <shreya.parse22@gmail.com>
2025-12-02 14:49:09 +0530

    Django Rest API tutorial: Nested_Serializers, SerializerMethodField and Serializer Relations

--- 29_Django_Rest_Framework/mysite/api/views.py
+++ 29_Django_Rest_Framework/mysite/api/views.py
@@ -1,10 +1,9 @@
 #from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
 
 @api_view(['GET'])
 def product_list(request):
@@ -16,4 +15,10 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serialzer = ProductSerializer(product)
+    return Response(serialzer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serialzer = OrderSerializer(orders, many=True)
     return Response(serialzer.data)
